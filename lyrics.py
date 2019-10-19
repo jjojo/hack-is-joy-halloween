4f2674964a32078138cf160517caef4519065f16
Jonas Johansson <jan.jonas.johansson@gmail.com>
2019-10-19 21:20:57 +0200

    end of hackday commit

---
+++ lyrics.py
@@ -0,0 +1,97 @@
+frame0 = \
+"                                                                \n" + \
+"                                                                \n"
+
+frame1 = \
+"          We clawed, we chained, our hearts in vain             \n" + \
+"          We jumped, never asking why                           \n"
+
+frame2 = \
+"          We kissed, I fell under your spell                    \n" + \
+"          A love no one could deny                              \n"
+
+frame3 = \
+"          Don't you ever say I just walked away                 \n" + \
+"          I will always want you                                \n"
+
+frame4 = \
+"          I can't live a lie, running for my life               \n" + \
+"          I will always want you                                \n"
+
+frame5 = \
+"          I came in like a wrecking ball                        \n" + \
+"          I never hit so hard in love                           \n"
+
+frame6 = \
+"          All I wanted was to break your walls                  \n" + \
+"          All you ever did was break me                         \n"
+
+frame7 = \
+"          Yeah, you wreck me                                    \n" + \
+"                                                                \n"
+
+frame8 = \
+"          I put you high up in the sky                          \n" + \
+"          And now, you're not coming down                       \n"
+
+frame9 = \
+"          It slowly turned, you let me burn                     \n" + \
+"          And now, we're ashes on the ground                    \n"
+
+frame10 = \
+"          Don't you ever say I just walked away                 \n" + \
+"          I will always want you                                \n"
+
+frame11 = \
+"          I can't live a lie, running for my life               \n" + \
+"          I will always want you                                 \n"
+
+frame12 = \
+"          I came in like a wrecking ball               \n" + \
+"          Yeah, I just closed my eyes and swung                                 \n"
+
+frame13 = \
+"          Left me crouching in a blaze and fall               \n" + \
+"          All you ever did was break me                                 \n"
+
+frame14 = \
+"          I never meant to start a war               \n" + \
+"          I just wanted you to let me in                                 \n"
+
+frame15 = \
+"          And instead of using force               \n" + \
+"          I guess I should've let you in                                 \n"
+
+frame16 = \
+"          I guess I should've let you in               \n" + \
+"                                           \n"
+
+frames = {
+  '0.0': frame0,
+  '9.0': frame1,
+  '17.0': frame2,
+  '25.0': frame3,
+  '33.0': frame4,
+  '43.0': frame5,
+  '49.0': frame6,
+  '59.0': frame7,
+  '63.0': frame8,
+  '70.0': frame9,
+  '79.0': frame10,
+  '86.0': frame11,
+  '94.0': frame5,
+  '102.0': frame6,
+  '110.0': frame12,
+  '118.0': frame13,
+  '126.0': frame7,
+  '133.0': frame14,
+  '141.0': frame15,
+  '150.0': frame14,
+  '157.0': frame16,
+  '163.0': frame10,
+  '172.0': frame5,
+  '190.0': frame6,
+  '198.0': frame12,
+  '206.0': frame13,
+  '217.0': frame7,
+}
---
+++ terminal_animation.py
@@ -0,0 +1,54 @@
+import time
+import os
+import playsound
+import footTap
+import footTapAndHeadBup
+import faceMimeing
+import wreckingball
+import hammerSkeletonSinging
+import lyrics
+
+playsound.playsound('Miley_Cyrus_Wrecking_Ball.mp3', False)
+startTime = time.time()
+previous = lyrics.frames[str(0.0)]
+
+def printLyricLine(time):
+    global previous
+    try:
+        previous = lyrics.frames[str(time)]
+        print(lyrics.frames[str(time)])
+    except:
+        print(previous)
+    
+
+def animate(frames, duration, cycles):
+    """Iterate through the frames, printing then clearing each one to create an animation."""
+    count = 0
+    
+    while count < cycles:
+        for frame in frames:
+            print(frame)
+            printLyricLine(round(time.time() - startTime))
+            time.sleep(duration)
+            print(os.system('clear'))
+        count = count + 1
+
+
+time.sleep(8.5)
+animate(faceMimeing.frames, 0.3, 16)
+animate(wreckingball.frames, 0.15, 5)
+animate([wreckingball.frames[22]], 0.3, 7)
+animate(hammerSkeletonSinging.frames, 0.5, 30 )
+animate(wreckingball.frames, 0.15, 10)
+animate(hammerSkeletonSinging.frames, 0.5, 31 )
+animate(faceMimeing.frames, 0.3, 5)
+animate(wreckingball.frames, 0.15, 13)
+animate([wreckingball.frames[22]], 0.3, 100)
+
+
+
+
+
+
+#animate(footTap.frames, 0.3, 4)
+#animate(footTapAndHeadBup.frames, 0.3, 4)
